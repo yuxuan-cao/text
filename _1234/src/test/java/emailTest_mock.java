73c2601255b7142c3fa3667828ca5c97b3f1ea8f
yuxuan-cao <74945149+yuxuan-cao@users.noreply.github.com>
2020-11-28 11:11:03 +0800

    Add files via upload

--- _1234/src/test/java/emailTest_mock.java
+++ _1234/src/test/java/emailTest_mock.java
@@ -21,14 +21,17 @@ public class emailTest_mock {
         //System.out.println(list.get(0));
         /* 没有设置则返回 null */
        // System.out.println(list.get(1));
-        email list = mock(email.class);
-      //  email spy = spy(list);
-       list.prepare("997845792@qq.com");
+        email list = new email();
+        //email spy = spy(list);
+       // email list = spy(email.class);
+        email spy = spy(list);
+       spy.prepare("997845792@qq.com");
      //   System.out.println(0);
-        list.set_email("test","test1","this is test","this is test1");
+        spy.set_email("test","test1","this is test","this is test1");
       //  System.out.println(0);
         //doNothing().when(spy).send_email();
-        doThrow(new RuntimeException("异常")).when(list).send_email();
+        assertEquals("test1",spy.title);
+        doThrow(new RuntimeException("异常")).when(spy).send_email();
 //use mock object
         //when(mockedList.prepare("99")).thenReturn();
         //verify(mockedList).send_email();
